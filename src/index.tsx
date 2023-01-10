fa26264c57bbd94e3985fa181fcc07f0d0047516
GabbereX <[gabberex@mail.ru]>
2023-01-10 22:24:39 +0300

    adaptation to ie11

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,16 @@
+import 'react-app-polyfill/ie11'
+import 'react-app-polyfill/stable'
+
+import cssVars from 'css-vars-ponyfill'
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import { Container } from './components/views/Container'
 
 import './assets/styles/styles.global.scss'
+
+cssVars()
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
